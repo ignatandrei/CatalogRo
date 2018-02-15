3d3386ec7623077d55398c5426fffca8bd274f7b
ignatandrei <ignatandrei@yahoo.com>
2018-02-15 06:47:37 +0200

    text modif

--- Applications/CatalogAPI/CatalogAPI/Startup.cs
+++ Applications/CatalogAPI/CatalogAPI/Startup.cs
@@ -39,7 +39,7 @@ namespace CatalogAPI
             Seed(services.BuildServiceProvider().GetRequiredService<CatalogROContext>());
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "Is This Taxi Legal API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "Catalog RO API", Version = "v1" });
                 //var filePath = Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "TaxiWebAndAPI.xml");
                 //c.IncludeXmlComments(filePath);
             });
