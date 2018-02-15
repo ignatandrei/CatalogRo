9ab53e5c307bbd3cd87d48467656a8adeeece356
ignatandrei <ignatandrei@yahoo.com>
2018-02-15 06:25:02 +0200

    default files

--- Applications/CatalogAPI/CatalogAPI/Startup.cs
+++ Applications/CatalogAPI/CatalogAPI/Startup.cs
@@ -94,15 +94,15 @@ namespace CatalogAPI
             }
             
             
-            //app.UseDefaultFiles();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseMvc(routes =>
-            {
+            //app.UseMvc(routes =>
+            //{
 
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
 
 
             //app.UseDirectoryBrowser(new DirectoryBrowserOptions
