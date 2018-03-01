12154fc3b900849fd979b9f06c1ed792eaadaab4
ignatandrei <ignatandrei@yahoo.com>
2018-03-01 16:12:32 +0200

    added categories to root

--- Applications/CatalogAPI/CatalogHtml/build.bat
+++ Applications/CatalogAPI/CatalogHtml/build.bat
@@ -1,2 +1,4 @@
-call ng build --prod --build-optimizer
+rem call ng build --prod --build-optimizer
 robocopy . D:\github\CatalogRo\Applications\CatalogAPI\CatalogHtml  /MIR /XD node_modules .git .vs
+
+robocopy dist D:\github\CatalogRo\Applications\CatalogAPI\CatalogAPI\wwwroot  /MIR /XD node_modules .git .vs
