6405906c82fc6fcce5e93f7fb02d1d7982a69f09
ignatandrei <ignatandrei@yahoo.com>
2018-03-01 16:20:16 +0200

    piushing values

--- Applications/CatalogAPI/CatalogAPI/Controllers/ValuesController.cs
+++ Applications/CatalogAPI/CatalogAPI/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace CatalogAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value31", "value22" };
         }
 
         // GET api/values/5
